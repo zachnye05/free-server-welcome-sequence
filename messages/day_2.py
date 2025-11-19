2c110c55ffdf9cbc0339e99f339b4b0c9e4405a6
zachnye05 <zach@discordlaunch.com>
2025-11-19 14:36:19 -0700

    Update day_2.py

--- messages/day_2.py
+++ messages/day_2.py
@@ -1,26 +1,26 @@
 import discord
+from typing import Tuple, List, Optional
 
-BANNER_URL = "https://media.discordapp.net/attachments/1436108078612484189/1436115777265598555/image.png"
+BANNER_URL = "https://cdn.discordapp.com/attachments/1435678774921269432/1440814266021187765/image.png"
+IMAGE_URL = "https://cdn.discordapp.com/attachments/1436108078612484189/1436115777265598555/image.png"
 
-def get_message(join_url: str):
-    embed = discord.Embed(
+def build_embed(join_url: str) -> Tuple[List[discord.Embed], Optional[discord.ui.View]]:
+    # --- Embed 1: Banner only ---
+    banner_embed = discord.Embed(color=0x2b2d31)  # neutral dark color
+    banner_embed.set_image(url=BANNER_URL)
+
+    # --- Embed 2: Main message ---
+    main_embed = discord.Embed(
         title="WALMART SELLING MACBOOKS FOR $23<a:PartyBear:774254653197647892>",
         description=(
             "Walmart is marking down Macbook Airs as low as **$23** at select stores.\n\n"
-            "Our software lets Divine members check **all stores within 50 miles** of their home "
-            "to instantly see whether their stores have the deal.\n\n"
-            "Want access?👇"
+            "Our software lets Divine members check *all* stores within 50 miles of their home for the deal.\n\n"
+            "Want access?👇\n\n"
+            f"[**CLAIM YOUR FREE WEEK NOW**]({join_url})"
         ),
         color=0x5865F2
     )
 
-    embed.set_image(url=BANNER_URL)
+    main_embed.set_image(url=IMAGE_URL)
 
-    view = discord.ui.View()
-    view.add_item(discord.ui.Button(
-        label="CLAIM YOUR FREE WEEK NOW",
-        url=join_url,
-        style=discord.ButtonStyle.link
-    ))
-
-    return embed, view
+    return [banner_embed, main_embed], None
